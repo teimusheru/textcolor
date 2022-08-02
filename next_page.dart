77407537080ae8c45fd460d3eec83f6d7f1e870b
広田　響 <35335011+teimusheru@users.noreply.github.com>
2022-08-02 12:02:05 +0900

    文字のフォント変化

---
+++ next_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget {
+  NextPage(this.name);
+  final String name;
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('次の画面'),
+      ),
+      body: Container(
+          height: double.infinity,
+          color: Colors.red,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            Text(name),
+             Center(
+
+             child: RaisedButton(
+               child: Text('戻る'),
+             onPressed: () {
+               Navigator.pop(context);
+
+        },
+    ),
+    ),
+      ],
+    )
+    ),
+    );
+}
+}
